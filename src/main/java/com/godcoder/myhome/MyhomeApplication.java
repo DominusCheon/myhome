1b8dd12695c8994c00cc8d6683440b7fc5f73e93
miCheon <miCheon@nsam.co.kr>
2021-01-29 00:22:44 +0900

    second

--- src/main/java/com/godcoder/myhome/MyhomeApplication.java
+++ src/main/java/com/godcoder/myhome/MyhomeApplication.java
@@ -2,12 +2,12 @@ package com.godcoder.myhome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MyhomeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyhomeApplication.class, args);
-	}
+	public static void main(String[] args) { SpringApplication.run(MyhomeApplication.class, args); }
+
 
 }
